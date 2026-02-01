b947617746aefdc93dbe9254805bb0f8e6f0dd2e
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2026-02-01 23:40:05 +0300

    ngrok changes

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = "http://localhost:8000";
-export const API_BASE_URL = "https://9590fcf1368a.ngrok-free.app";
-// export const API_BASE_URL = "https://leaktubeservice.onrender.com";
+// export const API_BASE_URL = "https://9590fcf1368a.ngrok-free.app";
+export const API_BASE_URL = "https://leaktubeservice.onrender.com";
 
 export const getApiUrl = (endpoint: string) => {
     return `${API_BASE_URL}${endpoint.startsWith('/') ? endpoint : `/${endpoint}`}`;
